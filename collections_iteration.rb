3763a23ffef58f38676028c2161e9e6a6d67054c
vioif <ireneaflores3@gmail.com>
2017-12-15 15:47:43 -0500

    first exercise complete

---
+++ collections_iteration.rb
@@ -0,0 +1,54 @@
+# Exercise 0
+
+# colors
+
+fav_colours = ["pink", "blue", "red"]
+
+# age
+
+age = [23, 22, 21, 20]
+
+# coin flip
+
+coin_flip = ["heads", "heads", "heads", "tails", "heads"]
+
+# favourite artists
+
+artists = ["jason aldean", "dvsn", "darren styles"]
+
+# colors as symbomls
+
+fav_symbols = [:pink, :blue, :red]
+
+# words and definitions
+
+definitions = {
+  :girl => "a female child from birth to adulthood",
+  :boy => "a male child from birth to adulthood",
+  :gender => "the behavioral, cultural, or psychological traits typically associated with one sex"
+}
+
+# favourite movie names and year of creation
+
+movies = {
+  :movie1 => "Mean Girls [2014]",
+  :movie2 => "Finding Nemo [2003]",
+  :movie3 => "The Breakfast Club [1985]"
+}
+
+# cities around the world and their population
+
+cities = {
+  :Toronto => "[2.8] million people",
+  :sydney => "[4.02] million people",
+  :Los_Angeles => "[3.9] million people"
+}
+
+# names of friends and their ages
+
+names_ages = {
+  :Irenea => "[23]",
+  :Noelle => "[22]",
+  :Alek   => "[21]",
+  :Taylor => "[20]"
+}
